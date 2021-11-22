b05f9be0f8f28cbabca6d5b4c08660abae78185e
bosscharlie <878574650@qq.com>
2021-11-22 11:25:53 +0800

    add element-ui

--- backend/webapp/views.py
+++ backend/webapp/views.py
@@ -1,3 +1,4 @@
+from django.db import reset_queries
 from django.shortcuts import render,HttpResponse
 from django.http import JsonResponse
 from . import models
@@ -9,4 +10,13 @@ def show(request):
     except Exception as e:
         res["msg"]=str(e)
         res["status"]=1
+    return JsonResponse(res)
+
+def add(request):
+    res={"status":1,"msg":""}
+    if request.method=="POST":
+        name=request.POST.get("post_name")
+        models.Sample.objects.create(name=name)
+        res["status"]=0
+        res["msg"]="success"
     return JsonResponse(res)
